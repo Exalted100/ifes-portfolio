ff9985d80b4102340d2620e68f57af9324c4c3a0
Fawaz Haroun <harounayomikun@gmail.com>
2021-10-08 18:41:29 +0100

    commit

--- src/Components/firstDiv.js
+++ src/Components/firstDiv.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import "./styles/firstDiv.css";
 
 const FirstDiv = () => {
     return (
-        <div>
-            First component
+        <>
+        <nav>
+            <ul className="nav-list">
+                <li>About</li>
+                <li>Skills</li>
+                <li>Work</li>
+                <li>Resume</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+        <div className="firstdiv-container">
+            <div className="hello">HELLO!</div>
+            <div className="intro">I'M IFE</div>
+            <div className="role">A UI/UX DESIGNER</div>
         </div>
+        <p className="scroll">SCROLL TO VIEW</p>
+        </>
     )
 }
 

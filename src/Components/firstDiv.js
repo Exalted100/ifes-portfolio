a4e6ec8f6c55d2fce883906fce27745951b1c9d0
Gabriel Abubakar <gabrielogrima@gmail.com>
2022-04-02 21:37:11 +0100

    update

--- src/Components/firstDiv.js
+++ src/Components/firstDiv.js
@@ -4,21 +4,21 @@ import "./styles/firstDiv.css";
 const FirstDiv = () => {
     return (
         <>
-        <nav className="nav-container">
-            <ul className="nav-list">
-                <li><a className="nav-link" href="#about">About</a></li>
-                <li><a className="nav-link" href="#skills">Skills</a></li>
-                <li><a className="nav-link" href="#work">Work</a></li>
-                <li><a className="nav-link" href="#resume">Resume</a></li>
-                <li><a className="nav-link" href="#contact">Contact</a></li>
-            </ul>
-        </nav>
-        <div className="firstdiv-container">
-            <div className="hello">HELLO!</div>
-            <div className="intro">I'M IFE</div>
-            <div className="role">A UI/UX DESIGNER</div>
-        </div>
-        <p className="scroll">SCROLL TO VIEW</p>
+            <nav className="nav-container">
+                <ul className="nav-list">
+                    <li><a className="nav-link" href="#about">About</a></li>
+                    <li><a className="nav-link" href="#skills">Skills</a></li>
+                    <li><a className="nav-link" href="#work">Work</a></li>
+                    <li><a className="nav-link" href="#resume">Resume</a></li>
+                    <li><a className="nav-link" href="#contact">Contact</a></li>
+                </ul>
+            </nav>
+            <div className="firstdiv-container">
+                <div className="hello">HELLO!</div>
+                <div className="intro">I'M IFE</div>
+                <div className="role">A PRODUCT DESIGNER</div>
+            </div>
+            <p className="scroll">SCROLL TO VIEW</p>
         </>
     )
 }
